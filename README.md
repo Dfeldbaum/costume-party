679abb712e6664d4f94f0922bac02079b4449393
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro-2.local>
2016-10-25 14:19:16 -0500

    added tables

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # costume-party
 
-## SQL commands 
+## 1. SQL commands 
 we create a specific user in the db for a project for security purposes
 
 ```
@@ -8,6 +8,40 @@ create database costume_party;
 create user 'l33tdba'@'localhost' identified by 'yellowpencil';
 grant all privileges on costume_party.* to 'l33tdba'@'localhost';
 ```
+## 2. Environment Variables
+Our environment variables are stored in .env byt may also be declared as needed
+
+```
+DB_USER=l33tdba
+DB_PWD=yellowpencil
+DB_NAME=costume_party
+DB_TYPE=mysql
+DB_SERVER=localhost
+```
+## 3. Database Connect
+Database connection is defined inside of db.js. It contains all code needed
+
+``` js
+// ./models/db.js
+'use strict';
 
 
+// loads environment variables from .env
+require('dotenv').config();
 
+// knex is a database adapter for node
+// npm install knex && npm install lodash
+// lodash is a dependency of knex
+
+var db = require('knex')({
+	client: process.env.DB_TYPE, 
+	connection: {
+		host: process.env.DB_SERVER,
+		user: process.env.DB_USER,
+		password: process.env.DB_PWD,
+		database: process.env.DB_NAME
+	}
+});	
+
+module.exports = db;
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,67 @@
 'use strict'
 
 var gulp = require('gulp');
-var watch = require('gulp-watch');
+// var watch = require('gulp-watch'); // remove bc don't need
 var nodemon = require('gulp-nodemon');
+var db = require('./models/db');
 
 
 
 // section to create SQL tables
+// (create table parties (id into not null auto_increment...)
+var SQL_TABLE_CREATE_PARTIES = "create table parties" + 
+	"(id int not null auto_increment," +
+	"name varchar(50) not null," +
+	"host_name varchar(20)," +
+	"location varchar(255) not null," +
+	"primary key (id)" +
+	");";
+var SQL_TABLE_DROP_PARTIES = "drop table parties;";
+var SQL_TABLE_SELECT_ALL_PARTIES = "select * from parties;";
 
+
+
+
+gulp.task('db_create_tables', function(){
+	console.log('creating database tables...');
+	var tag = 'SQL'; // just for logging purposes only 
+	// callback for an asynch method
+	function createTableCallback(response) {
+	console.log(tag + ' Table creation completed.');
+	console.log(response);	
+	}
+
+	db.raw(SQL_TABLE_CREATE_PARTIES).then(createTableCallback);
+
+});
+
+
+gulp.task('db_drop_tables', function(){
+	console.log('Dropping database tables...');
+
+	function dropTableCallback(response){
+		console.log(response);
+	}
+	db.raw(SQL_TABLE_DROP_PARTIES).then(dropTableCallback);
+
+});
+
+
+gulp.task('db_select_all_parties', function() {
+	console.log('Selecting all parties');
+	//callback response
+	function selectAllPartiesCallback(response){
+		console.log(response)
+	}
+
+	db.raw(SQL_TABLE_SELECT_ALL_PARTIES).then(selectAllPartiesCallback);
+
+});
 
 
 // section to drop SQL tables
+
+
 
 
 // watch for changes!
